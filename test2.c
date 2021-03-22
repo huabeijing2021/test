ea9c434c2e47d97f3d04201a0e54957e2c37582f
huabeijing2021 <huabeijing2021@hotmail.com>
2021-03-22 19:57:58 +0800

    test

--- test2.c
+++ test2.c
@@ -7,5 +7,12 @@ int main()
     printf("%d\n",age);
     long num = 1000;
     printf("%d\n",num);
+    printf ("%d\n",sizeof(char));
+    printf ("%d\n",sizeof(int));
+    printf ("%d\n",sizeof(long));
+    printf ("%d\n",sizeof(short));
+    printf ("%d\n",sizeof(long long));
+    printf ("%d\n",sizeof(float));
+    printf ("%d\n",sizeof(double));
     return 0;
 } 
